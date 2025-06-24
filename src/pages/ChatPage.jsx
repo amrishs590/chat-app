863b9cc76ce4b4a6a579552ffcabb453b4e3a425
amrishs590 <amrishs590@gmail.com>
2025-06-24 11:57:35 +0530

    Final commit

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { supabase } from "../supabaseClient";
 import { useNavigate } from "react-router-dom";
-import React, { useRef } from "react";
 import "./ChatPage.css";
 
 const ChatPage = () => {
